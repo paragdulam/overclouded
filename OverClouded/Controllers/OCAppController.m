76f23e8115c2b0582fcb64a3b4d343f43c7cab1a
Parag Dulam <paragdulam@gmail.com>
2014-11-26 19:16:46 +0530

    Adding Same Account Bug Solved

--- OverClouded/Controllers/OCAppController.m
+++ OverClouded/Controllers/OCAppController.m
@@ -32,40 +32,58 @@
     NSURL *accountURL = nil;
     NSDictionary *params = nil;
     NSMutableURLRequest *request = nil;
+    __block BOOL isAlreadyStored = NO;
+
     switch (type) {
         case DROPBOX:
         {
-            NSString *accountURLString = @"https://api.dropbox.com/1/account/info?";
-            NSMutableString *finalURLString = [NSMutableString stringWithString:accountURLString];
-            params = @{@"access_token":[authDict objectForKey:@"access_token"]};
-            NSArray *keys = [params allKeys];
-            for (NSString *key in keys) {
-                [finalURLString appendFormat:@"%@=%@",key,[params objectForKey:key]];
-                if (![[keys lastObject] isEqualToString:key]) {
-                    [finalURLString appendString:@"&"];
+            NSString *userId = [authDict objectForKey:@"uid"];
+            [[self class] getAllAccountsWithCompletionBlock:^(id response, NSError *error) {
+                NSArray *accounts = (NSArray *)response;
+                NSPredicate *predicate = [NSPredicate predicateWithFormat:@"userId == %@",userId];
+                NSArray *results = [accounts filteredArrayUsingPredicate:predicate];
+                isAlreadyStored = [results count] ? YES : NO;
+            }];
+
+            if (!isAlreadyStored) {
+                NSString *accountURLString = @"https://api.dropbox.com/1/account/info?";
+                NSMutableString *finalURLString = [NSMutableString stringWithString:accountURLString];
+                params = @{@"access_token":[authDict objectForKey:@"access_token"]};
+                NSArray *keys = [params allKeys];
+                for (NSString *key in keys) {
+                    [finalURLString appendFormat:@"%@=%@",key,[params objectForKey:key]];
+                    if (![[keys lastObject] isEqualToString:key]) {
+                        [finalURLString appendString:@"&"];
+                    }
                 }
+                accountURL = [NSURL URLWithString:finalURLString];
+                request = [NSMutableURLRequest requestWithURL:accountURL];
             }
-            accountURL = [NSURL URLWithString:finalURLString];
-            request = [NSMutableURLRequest requestWithURL:accountURL];
         }
             break;
         default:
             break;
     }
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        //cache it
-        NSDictionary *accountData = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
-        NSLog(@"accountData %@",accountData);
-        NSMutableDictionary *finalAccountDict = [[NSMutableDictionary alloc] init];
-        [finalAccountDict addEntriesFromDictionary:accountData];
-        [finalAccountDict addEntriesFromDictionary:authDict];
-        OCAccount *account = [[OCAccount alloc] initWithAccount:finalAccountDict
-                                                         ofType:type];
-        OCAccountController *accountController = [[OCAccountController alloc] initWithAccount:account];
-        completionhandler(account,nil);
-        [accountController saveWithCompletionBlock:^(OCAccount *account) {
+    if (!isAlreadyStored) {
+        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            //cache it
+            NSDictionary *accountData = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+            NSMutableDictionary *finalAccountDict = [[NSMutableDictionary alloc] init];
+            [finalAccountDict addEntriesFromDictionary:accountData];
+            [finalAccountDict addEntriesFromDictionary:authDict];
+            OCAccount *account = [[OCAccount alloc] initWithAccount:finalAccountDict
+                                                             ofType:type];
+            OCAccountController *accountController = [[OCAccountController alloc] initWithAccount:account];
+            completionhandler(account,nil);
+            [accountController saveWithCompletionBlock:^(OCAccount *account) {
+            }];
         }];
-    }];
+    } else {
+        NSError *error = [NSError errorWithDomain:@"accounts.already_exists"
+                                             code:9999
+                                         userInfo:nil];
+        completionhandler(nil,error);
+    }
 }
 
 
